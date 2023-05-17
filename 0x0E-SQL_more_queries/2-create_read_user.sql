f384d047cf1481c812b61b372ae58e80761e10dd
StanleyGemstone <stanleynduchi@gmail.com>
2023-05-17 22:37:44 +0100

    Task 3

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the MySQL database hbtn_0d_2
+-- script that creates the MySQL user user_0d_2
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT PRIVILEGES SELECT ON * . * TO 'user_0d_2'@'localhost';
