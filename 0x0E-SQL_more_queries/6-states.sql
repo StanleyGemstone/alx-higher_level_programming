cf77399b792d21d2356b1abcc457ad7871f4bf60
StanleyGemstone <stanleynduchi@gmail.com>
2023-05-17 23:20:25 +0100

    zTask 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL database hbtn_0d_usa
+-- script that creates the MySQL user states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS state (id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL);
