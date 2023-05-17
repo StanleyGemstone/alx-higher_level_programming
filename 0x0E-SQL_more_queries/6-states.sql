49e75722e9d1f5ab88c2dbecc8c0c843c18e5676
StanleyGemstone <stanleynduchi@gmail.com>
2023-05-17 23:29:48 +0100

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,7 @@
--- script that creates the MySQL database hbtn_0d_usa
--- script that creates the MySQL user states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'state' (id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256 NOT NULL);
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
