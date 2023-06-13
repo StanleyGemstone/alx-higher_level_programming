eaf044d2329e9e0c579cbf2d0bcbe3be60674954
StanleyGemstone <stanleynduchi@gmail.com>
2023-06-13 20:27:41 +0100

    Task 10 edited

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,14 +3,10 @@ const factorial = (n) => {
  if (isNaN(n)) {
      return 1;
   }
-
-
   if (n === 0) {
     return 1;
   }
-
   return n * factorial(n - 1);
 };
-
 const input = parseInt(process.argv[2]);
 console.log(factorial(input));
