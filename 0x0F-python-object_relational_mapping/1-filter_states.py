c2508cb17df46cd00170e7cf5306ece158c672f8
StanleyGemstone <stanleynduchi@gmail.com>
2023-06-18 19:59:40 +0100

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-'''File Doc'''
+'''
+This script connects to a MySQL database and retrieves data from the "states" table.
+
+Usage: python script_name.py <username> <password> <database_name>
+'''
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    '''init by filename'''
+    '''
+    Entry point of the script.
+    '''
+
+    # Establish a connection to the MySQL database
     database = MySQLdb.connect(
         host='localhost',
         port=3306,
@@ -14,11 +22,23 @@ if __name__ == '__main__':
         db=argv[3],
         charset='utf8'
     )
+
+    # Create a cursor object to execute SQL queries
     cursor = database.cursor()
+
+    # Define the SQL query to select all rows from the "states" table and order them by id in ascending order
     query = 'SELECT * FROM states ORDER BY id ASC'
+
+    # Execute the SQL query
     cursor.execute(query)
+
+    # Fetch all the rows returned by the query
     states = cursor.fetchall()
+
+    # Iterate over each state and print it
     for state in states:
         print(state)
+
+    # Close the cursor and database connection
     cursor.close()
     database.close()
