b839522a3bc899756c70676f8c7a646c2e69383f
StanleyGemstone <stanleynduchi@gmail.com>
2023-06-29 09:46:05 +0100

    0x10. Python - Network #0

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that displays all HTTP methods the server accepts
-curl -sI "$1" | grep "Allow" | cut -d' ' -f 2-
+# script that sends DELETE request and display the response
+curl -s "$1" -X DELETE
