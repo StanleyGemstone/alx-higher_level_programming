1c461b1390d1fc6d607cfd73b14f50cc596f3519
StanleyGemstone <stanleynduchi@gmail.com>
2023-06-30 17:07:08 +0100

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+"""  fetches https://intranet.hbtn.io/status  """
 import urllib.request
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
-        body = reply.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
